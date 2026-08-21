1184d5e46afff023d9a80026077670919546a4a6
San-fx <aramuslim888@gmail.com>
2026-08-21 03:19:37 +0300

    Fix formatting in bot.py for trading message

--- bot.py
+++ bot.py
@@ -56,7 +56,9 @@ def check_sources():
                             f"🔗 [تەواوی بابەتەکە بخوێنەوە]({link})\n\n"
                             f"----------------------------------\n"
                             f"هەواڵ و شیکاری ئابووری 📊\n"
-                            f"SAN FX TRADING"
+                            f"SAN FX TRADING\n\n"
+                            f"بۆ شیکاری ڕۆژانەی بازاڕە داراییەکان (ئاڵتون) ⬇️⬇️\n"
+                            f"https://t.me/money_ffo"
                         )
                         
                         send_telegram_message_to_channel(message)
