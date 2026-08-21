ad7505dfe0d4e01c9d3e501506348376d135c587
San-fx <aramuslim888@gmail.com>
2026-08-21 23:09:45 +0300

    Refactor Bloomberg source name and clean up message
    Updated the source name for Bloomberg and removed extra message lines.

--- bot.py
+++ bot.py
@@ -10,7 +10,7 @@ TELEGRAM_BOT_TOKEN = '8760352008:AAEAMs8aU3ZzgJrVNpFWLi-Tg_j2KCSbU9U'
 CHANNEL_ID = '@hawal_san'
 
 SOURCES = {
-    "Bloomberg Live": "https://feeds.bloomberg.com/markets/news.rss",
+    "Bloomberg": "https://feeds.bloomberg.com/markets/news.rss",
     "Forex Factory": "https://www.forexfactory.com/news/rss"
 }
 
@@ -56,9 +56,7 @@ def check_sources():
                             f"🔗 [تەواوی بابەتەکە بخوێنەوە]({link})\n\n"
                             f"----------------------------------\n"
                             f"هەواڵ و شیکاری ئابووری 📊\n"
-                            f"SAN FX TRADING\n\n"
-                            f"بۆ شیکاری ڕۆژانەی بازاڕە داراییەکان (ئاڵتون) ⬇️⬇️\n"
-                            f"https://t.me/money_ffo"
+                            f"SAN FX TRADING"
                         )
                         
                         send_telegram_message_to_channel(message)
